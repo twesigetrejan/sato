f68e536d7a995bc3ca43dfb4f34e0a15d2c501f5
twesigetrejan <trejantwesige19@gmail.com>
2024-08-04 23:46:35 +0300

    done

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    images: {
+        domains: [
+            'photos.fife.usercontent.google.com',
+            'media.licdn.com',
+            'images.pexels.com',
+        ],
+    },
+};
 
 export default nextConfig;
